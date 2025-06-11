46ff5cb41c09c6a24ccafb18e097859ee6dd6d8a
Natascha Blumer <blumernatascha@gmail.com>
2025-06-11 21:17:23 +0200

    added router and updated navigation and layout

--- natural-events-tracker/src/components/layout.jsx
+++ natural-events-tracker/src/components/layout.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import { Outlet } from "react-router-dom";
+import Navigation from './navigation'
 
 const Layout = () => {
+    const currentYear = new Date().getFullYear();
     return (
-        <div>Layout</div>
-    )
-}
+        <div className='App'>
+            <header className='App-header'>
+                <Navigation />
+            </header>
 
-export default Layout
+            <main className='layout-main-content'>
+                <Outlet />
+            </main>
+
+            <footer className='layout-footer'>
+                <p>© {currentYear} Natascha Blumer</p>
+            </footer>
+        </div>
+    );
+};
+
+export default Layout;
--- natural-events-tracker/src/components/navigation.jsx
+++ natural-events-tracker/src/components/navigation.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { Link } from "react-router-dom";
 
 const Navigation = () => {
     return (
-        <div>Navigation</div>
+        <nav className='layout-header-nav'>
+            <Link to='/'>Home</Link>
+            <Link to='/live-events'>Live Events</Link>
+            <Link to='/archive'>Archive</Link>
+            <Link to='/custom-events'>Custom Events</Link>
+        </nav>
     )
 }
 

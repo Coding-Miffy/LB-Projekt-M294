ce0de1ef2560f33d397961b77f31284e2db4c186
Natascha Blumer <blumernatascha@gmail.com>
2025-06-11 20:48:20 +0200

    added pages and components

---
+++ natural-events-tracker/src/components/custom-event-form.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CustomEventForm = () => {
+    return (
+        <div>CustomEventForm</div>
+    )
+}
+
+export default CustomEventForm
---
+++ natural-events-tracker/src/pages/CustomEvents.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CustomEvents = () => {
+    return (
+        <div>CustomEvents</div>
+    )
+}
+
+export default CustomEvents
